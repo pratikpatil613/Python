4cb4e5229a34a02262dc77c17ead85a696b24745
Pratik <ppatil@unobot.ai>
2021-04-03 13:52:39 +0530

    numTO1

--- Square/square.py
+++ Square/square.py
@@ -3,7 +3,7 @@ number = int(input("enter number\n"))
 
 def print_square(number):
     for num in range(1, number+1):
-        print(num,' * ',num,' = ', num*num)
+        print(1,' * ',num,' = ', num*num)
 
 
 print_square(number)
